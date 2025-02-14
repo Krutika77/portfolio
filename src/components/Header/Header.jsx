e3acd151fa84456d41bc28cb3d3c0a1b3185c961
Krutika77 <40793266+Krutika77@users.noreply.github.com>
2025-02-13 20:52:05 -0500

    Temporary responsive header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,35 @@ import "./Header.scss";
 
 function Header() {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="header">
+      <div className="header__wrapper">
+        <h1 className="header__logo">Krutika Desai</h1>
+        <nav className="header__nav">
+          <ul className="header__nav-list">
+            <li className="header__nav-item">
+              <a href="#about" className="header__nav-link">
+                About
+              </a>
+            </li>
+            <li className="header__nav-item">
+              <a href="#projects" className="header__nav-link">
+                Projects
+              </a>
+            </li>
+            <li className="header__nav-item">
+              <a href="#contact" className="header__nav-link">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+        {/* Hamburger Menu (Mobile Only) */}
+        <button className="header__hamburger">
+          <span className="header__hamburger-line"></span>
+          <span className="header__hamburger-line"></span>
+          <span className="header__hamburger-line"></span>
+        </button>
+      </div>
     </header>
   );
 }
